95c4e18476b0ce8e2fa45f7a0156e502243c3f9f
abteilung6 <moeller.tilman@gmail.com>
2021-01-20 22:21:15 +0100

    add sample load instances with authorization

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { getInstancesStart } from '../slices/instances';
+import { setAuthorization } from '../utils/axiosConfig';
 
 export interface HomePageProps {}
 
 // eslint-disable-next-line no-empty-pattern
 export const HomePage: React.FC<HomePageProps> = ({}) => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    setAuthorization(
+      `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6InMwNTU4MTUxIn0.DDNBIrZ4P2zptZ10z8S1CJo94ihVFgL93qKk247H6WE`,
+    );
+    dispatch(getInstancesStart());
+  }, [dispatch]);
+
   return <>HomePage</>;
 };
 
