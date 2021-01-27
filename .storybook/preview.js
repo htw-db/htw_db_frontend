1973131ffa17ae508b19e5ff521c6053f16db846
abteilung6 <moeller.tilman@gmail.com>
2021-01-27 12:56:32 +0100

    configure storybook with imports

---
+++ .storybook/preview.js
@@ -0,0 +1,10 @@
+import '../src/assets/css/vendor/bootstrap.min.css';
+import '../src/assets/css/vendor/bootstrap.rtl.only.min.css';
+import '../src/assets/css/sass/themes/gogo.light.greenlime.scss';
+import '../src/assets/fonts/simple-line-icons/css/simple-line-icons.css';
+import '../src/assets/fonts/iconsmind-s/css/iconsminds.css';
+import 'react-perfect-scrollbar/dist/css/styles.css';
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+};
