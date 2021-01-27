eaae5ae622b65f6f9c104b05c749eab7c26f9a57
abteilung6 <moeller.tilman@gmail.com>
2021-01-27 13:10:10 +0100

    create menu reducer with selectors

--- src/reducers.ts
+++ src/reducers.ts
@@ -1,9 +1,11 @@
 import { combineReducers } from '@reduxjs/toolkit';
 
 import instanceReducer from './slices/instances';
+import menuReducer from './slices/menu';
 
 const combinedReducers = combineReducers({
   instances: instanceReducer,
+  menu: menuReducer,
 });
 
 export default combinedReducers;
