441b2d7a1976e40da860717de99978864bdfd101
abteilung6 <moeller.tilman@gmail.com>
2021-01-29 19:09:45 +0100

    add auth reducer

---
+++ src/slices/auth.ts
@@ -0,0 +1,64 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+import { LdapCredentialsInterface, ProfileInterface } from '../types';
+import { setAuthorization } from '../utils/axiosConfig';
+import { decodeJwt } from '../utils/jwtUtils';
+
+interface AuthState {
+  profile?: ProfileInterface;
+  token?: string;
+  isLoading: boolean;
+  error?: string;
+}
+
+const restoreInitialSate = (): AuthState => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    const profile = decodeJwt(token);
+    if (profile) {
+      return { isLoading: false, token, profile };
+    }
+  }
+  return { isLoading: false };
+};
+
+export const initialState: AuthState = restoreInitialSate();
+
+const AuthSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    loginStart(state, { payload }: PayloadAction<LdapCredentialsInterface>) {
+      state.isLoading = true;
+      state.error = undefined;
+      state.profile = undefined;
+    },
+    loginSuccess(state, { payload }: PayloadAction<string>) {
+      state.error = undefined;
+      state.token = payload;
+      const profile = decodeJwt(payload);
+      if (profile) {
+        state.profile = profile;
+        localStorage.setItem('token', payload);
+        setAuthorization(payload);
+      } else {
+        state.profile = undefined;
+        state.error = 'Invalid token';
+        localStorage.removeItem('token');
+        setAuthorization('');
+      }
+      state.isLoading = false;
+    },
+    loginFailed(state, action: PayloadAction<string>) {
+      state.error = action.payload;
+      state.profile = undefined;
+      localStorage.removeItem('token');
+      setAuthorization('');
+      state.isLoading = false;
+    },
+  },
+});
+
+export const { loginStart, loginSuccess, loginFailed } = AuthSlice.actions;
+
+export default AuthSlice.reducer;
