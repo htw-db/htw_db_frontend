178254b104acd11e4dc338bd13747536db3f36fb
abteilung6 <moeller.tilman@gmail.com>
2021-01-29 19:10:17 +0100

    add auth saga side effects for login

--- src/sagas/index.ts
+++ src/sagas/index.ts
@@ -1,7 +1,9 @@
 import { all, fork } from 'redux-saga/effects';
 
+import { authSaga } from './auth';
 import { instancesSaga } from './instances';
 
 export function* rootSaga() {
   yield all([fork(instancesSaga)]);
+  yield all([fork(authSaga)]);
 }
