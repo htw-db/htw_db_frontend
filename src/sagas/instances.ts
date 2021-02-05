807fc2f735a4a3c631abe06ea78f92bb6697a627
abteilung6 <moeller.tilman@gmail.com>
2021-02-05 17:19:58 +0100

    add instance side effect

--- src/sagas/instances.ts
+++ src/sagas/instances.ts
@@ -1,8 +1,16 @@
 import { call, put, takeLatest } from 'redux-saga/effects';
+import { PayloadAction } from '@reduxjs/toolkit';
 
-import { InstanceInterface } from '../types';
-import { getInstances } from '../api/instances';
-import { getInstancesStart, getInstancesSuccess, getInstancesFailed } from '../slices/instances';
+import { AddInstanceInterface, InstanceInterface } from '../types';
+import { addInstance, getInstances } from '../api/instances';
+import {
+  getInstancesStart,
+  getInstancesSuccess,
+  getInstancesFailed,
+  addInstanceStart,
+  addInstanceSuccess,
+  addInstanceFailed,
+} from '../slices/instances';
 
 function* handleGetInstances() {
   try {
@@ -14,6 +22,17 @@ function* handleGetInstances() {
   }
 }
 
+function* handleAddInstance(action: PayloadAction<AddInstanceInterface>) {
+  try {
+    const response = yield call(addInstance, action.payload);
+    const instance: InstanceInterface = response.data;
+    yield put(addInstanceSuccess(instance));
+  } catch (error) {
+    yield put(addInstanceFailed(error.toString()));
+  }
+}
+
 export function* instancesSaga() {
   yield takeLatest(getInstancesStart.type, handleGetInstances);
+  yield takeLatest(addInstanceStart.type, handleAddInstance);
 }
