0642c105992831f0162373896c7c79998c19cda1
abteilung6 <moeller.tilman@gmail.com>
2021-02-05 14:16:31 +0100

    add seed instances

---
+++ src/constants/instances.ts
@@ -0,0 +1,19 @@
+import { InstanceInterface } from '../types';
+
+export const instances: InstanceInterface[] = [
+  {
+    id: 1,
+    name: 'username_name1',
+    personId: 1,
+  },
+  {
+    id: 2,
+    name: 'username_name2',
+    personId: 1,
+  },
+  {
+    id: 2,
+    name: 'username_name3',
+    personId: 1,
+  },
+];
