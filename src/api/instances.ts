556a108e324cdecca32763d16ae5d0a47d4a3478
abteilung6 <moeller.tilman@gmail.com>
2021-02-05 17:11:53 +0100

    extend instance api with add operation

--- src/api/instances.ts
+++ src/api/instances.ts
@@ -1,5 +1,5 @@
 import { axiosInstance } from '../utils/axiosConfig';
-import { InstanceInterface } from '../types';
+import { AddInstanceInterface, InstanceInterface } from '../types';
 
 const { REACT_APP_ENDPOINT_INSTANCES } = process.env;
 const endpointInstances = REACT_APP_ENDPOINT_INSTANCES || '';
@@ -7,3 +7,7 @@ const endpointInstances = REACT_APP_ENDPOINT_INSTANCES || '';
 export const getInstances = async () =>
   // eslint-disable-next-line no-return-await
   await axiosInstance.get<InstanceInterface[]>(endpointInstances);
+
+// eslint-disable-next-line arrow-body-style
+export const addInstance = async (instance: AddInstanceInterface) =>
+  axiosInstance.post<InstanceInterface>(endpointInstances, instance);
