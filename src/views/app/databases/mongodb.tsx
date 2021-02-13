9828c134fe857cb39a24bd5f6143c8b9b410acc4
abteilung6 <moeller.tilman@gmail.com>
2021-02-13 18:55:29 +0100

    add future hints

--- src/views/app/databases/mongodb.tsx
+++ src/views/app/databases/mongodb.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 interface MongoDBProps {}
 
-const MongoDB: React.FC<MongoDBProps> = () => <>MongoDB</>;
+const MongoDB: React.FC<MongoDBProps> = () => <>MongoDB (Future)</>;
 
 export default MongoDB;
--- src/views/app/docs/changelog.tsx
+++ src/views/app/docs/changelog.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 interface ChangelogProps {}
 
-const Changelog: React.FC<ChangelogProps> = () => <>Changelog</>;
+const Changelog: React.FC<ChangelogProps> = () => <>Changelog (Future)</>;
 
 export default Changelog;
